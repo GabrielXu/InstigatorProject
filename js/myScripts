2a8beecd82d578a6dfc9e2cb83a4e2a7b3fe78b4
GabrielXu <ShuboXu@stmarksschool.org>
2017-03-07 20:29:59 -0500

    #1

---
+++ js/myScripts
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+alert("hi");
+    $("#Tw").hover( function(){
+    	
+
+    	$("#Tw").css("background-color","white");
+    });
+     $("#Tw").mouseout( function(){
+
+    	$("#Tw").css("background-color","#ffe6ff");
+    });
+     $("#Fa").hover( function(){
+    	
+
+    	$("#Fa").css("background-color","white");
+    });
+     $("#Fa").mouseout( function(){
+
+    	$("#Fa").css("background-color","#ffe6ff");
+    });
+     $("#In").hover( function(){
+    	
+
+    	$("#In").css("background-color","white");
+    });
+     $("#In").mouseout( function(){
+
+    	$("#In").css("background-color","#ffe6ff");
+    });
+    $("#Pi").hover( function(){
+    	
+
+    	$("#Pi").css("background-color","white");
+    });
+     $("#Pi").mouseout( function(){
+
+    	$("#Pi").css("background-color","#ffe6ff");
+    });  
+});
---
+++ js/myScripts.js
@@ -0,0 +1,90 @@
+
+
+
+
+$(document).ready(function() {
+
+    $(".LOGO").mouseover( function(){
+        
+        $('.dropdown').css("float","right");
+        $(".LOGO").css("width","20%");
+    });
+
+     $(".LOGO").mouseout(function(){
+
+        $(".LOGO").css("width","10%");
+        $('.dropdown').css("float","none");
+    });
+
+
+    $("#Tw").hover( function(){
+        
+
+        $("#Tw").css("background-color","white");
+    });
+     $("#Tw").mouseout( function(){
+
+        $("#Tw").css("background-color","#ffe6ff");
+    });
+     $("#Fa").hover( function(){
+        
+
+        $("#Fa").css("background-color","white");
+    });
+     $("#Fa").mouseout( function(){
+
+        $("#Fa").css("background-color","#ffe6ff");
+    });
+     $("#In").hover( function(){
+        
+
+        $("#In").css("background-color","white");
+    });
+     $("#In").mouseout( function(){
+
+        $("#In").css("background-color","#ffe6ff");
+    });
+    $("#Pi").hover( function(){
+        
+
+        $("#Pi").css("background-color","white");
+    });
+     $("#Pi").mouseout( function(){
+
+        $("#Pi").css("background-color","#ffe6ff");
+    });  
+
+        $("#AU").hover( function(){
+        
+
+        $("#AU").css("color","#ffe6ff");
+    });
+     $("#AU").mouseout( function(){
+
+        $("#AU").css("color","white");
+    });
+     
+
+     $("#PW").hover( function(){
+        
+
+        $("#PW").css("color","#ffe6ff");
+    });
+     $("#PW").mouseout( function(){
+
+        $("#PW").css("color","white");
+    });
+     $("#CF").hover( function(){
+        
+
+        $("#CF").css("color","#ffe6ff");
+    });
+     $("#CF").mouseout( function(){
+
+        $("#CF ").css("color","white");
+    });
+
+    
+
+      
+});
